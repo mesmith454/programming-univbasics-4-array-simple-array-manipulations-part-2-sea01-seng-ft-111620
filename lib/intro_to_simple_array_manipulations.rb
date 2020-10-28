1ca98f5ecc1fd117d61f004bc0ff9dad225bf915
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-28 21:45:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,9 @@ def using_concat(array1, array2)
   array1.concat array2 
 end
 
-def using_insert(array, element)
+def using_insert(array, "noodles")
   array = [1,2,3]
-  array.insert(4,element)
+  array.insert(4, "noodles")
   return array
 end
 
